fddab11c0e274103797131a9efaf26bb1e763ab9
Tanvir Hasan Tanim <79391254+tanim1823@users.noreply.github.com>
2024-10-31 10:03:24 +0600

    Update number-theory.cpp
    Prime Gen

--- number-theory.cpp
+++ number-theory.cpp
@@ -8,3 +8,47 @@
         }
     }
     dbg(cnt , lim+1);
+
+
+/// Seive Of Eratosthense
+/// Prime number
+const int mx = 1e8+123;
+// bool isPrime[mx];
+bitset<mx> isPrime;
+vector<int> primes;
+
+void primeGen(int n){
+    int sq = sqrt(n);
+    for(int i = 3; i <=n; i+=2) isPrime[i] = 1;
+    for(int i = 3; i<=sq;i+=2){
+        if(isPrime[i]){
+            for(int j = i*i; j<=n;j+=i){
+                isPrime[j] = 0;
+            }
+        }
+    }
+    isPrime[2] = 1;
+    primes.push_back(2);
+    for(int i = 3; i<=n; i+= 2){
+        if(isPrime[i]){
+            primes.push_back(i);
+        }
+    }
+}
+
+ int lim = 20;
+    primeGen(lim);
+    dbg(primes);
+
+/// End Of Prime Generation
+
+
+
+
+
+
+
+
+
+
+
