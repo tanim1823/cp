1c83178c236770f9fff124929b372c52bf0ddd59
Tanvir Hasan Tanim <79391254+tanim1823@users.noreply.github.com>
2025-03-27 15:01:32 +0600

    Create Subarray_Sums_II.cpp

---
+++ algorithm/partialsum/Subarray_Sums_II.cpp
@@ -0,0 +1,139 @@
+/// https://cses.fi/problemset/task/1661/
+///In the name of ALLAH
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define endl '\n'
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define YES; cout << "YES\n";
+#define NO; cout << "NO\n";
+#define yes; cout << "yes\n";
+#define no; cout << "no\n";
+ 
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define gcd(a,b) __gcd(a,b)
+#define sqr(a) ((a) * (a))
+ 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+ 
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+            return os << "(" << p.first << ", " << p.second << ")";
+}
+ 
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+            os << "{";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "}";
+}
+ 
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+ 
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+ 
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << it -> first << " = " << it -> second ;
+                                            }
+                    return os << "]";
+}
+ 
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+ 
+void faltu () {
+            cerr << endl;
+}
+ 
+template <typename T>
+void faltu( T a[], int n ) {
+            for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
+                cerr << endl;
+}
+ 
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) {
+            cerr << arg << ' ';
+                faltu(rest...);
+}
+const int mx = 2e5+123;
+vector<ll> v(mx);
+vector<ll> sum(mx);
+ 
+int main()
+{
+   optimize();
+   map<ll , int > cnt;
+   int n ,x;
+   cin >> n >> x;
+   v[0]=0;
+   cnt[0]++;
+   ll total = 0;
+    for(int i = 1;i<=n;i++) cin >> v[i];
+    for(int i = 1;i<=n;i++) {
+        sum[i] = (sum[i-1] + v[i]);
+ 
+    }
+     for(int i = 1;i<=n;i++){
+        ll diff = sum[i] - x;
+        total += cnt[diff];
+        cnt[sum[i]]++;
+     }
+     cout << total<<endl;
+ 
+ 
+ 
+    return 0;
+}
